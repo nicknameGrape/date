dfca2d661de64ace5a78d421f7e331fb41829bf5
Grant Rozier <grozier@mini.local>
2023-05-21 20:38:13 +0900

    visual update, interactive weather, improved japanese (thanks Haraguchi-san)

--- main.mjs
+++ main.mjs
@@ -2,62 +2,77 @@
 import fitText from "./js_modules/fitText.mjs";
 import fitImage from "./js_modules/fitImage.mjs";
 import HatDraw from "./js_modules/HatDraw.mjs";
+import Loader from "./js_modules/Loader.mjs";
+
+function drawDate() {
+	today = Date.now();
+	time = dateFormat(today, "h:MM TT");
+	//dateFormat(today, "h:MM TT, dddd, mmmm dS, yyyy");
+	divTime.innerHTML = time;
+	divDayE.innerHTML = day;
+	divDayJ.innerHTML = TRANSLATIONS[day];
+	divDateE.innerHTML = "<span>"+month+" <span>"+dateString.slice(0,2)+"<sup>"+dateString.slice(2,4)+"</sup></span></span>";
+	divDateJ.innerHTML = TRANSLATIONS[month]+dateNumber+"日";
+	divYear.innerHTML = year;
+	divSeasonE.innerHTML = season;
+	divSeasonJ.innerHTML = TRANSLATIONS[season];
+	context.clearRect(0, 0, weatherCanvas.width, weatherCanvas.height);
+	if (sunny.checked) {
+		fitImage(context, weather.sunny.img);
+	}
+	if (cloudy.checked) {
+		fitImage(context, weather.cloudy.img);
+	}
+	if (rainy.checked) {
+		fitImage(context, weather.cloudy.img);
+		fitImage(context, weather.rainy.img);
+	}
+	if (stormy.checked) {
+		fitImage(context, weather.stormy.img);
+		fitImage(context, weather.cloudy.img);
+	}
+	if (windy.checked) {
+		fitImage(context, weather.windy.img);
+	}
+	if (hot.checked) {
+		fitImage(context, weather.hot.img);
+	}
+	if (cold.checked) {
+		fitImage(context, weather.cold.img);
+	}
+	if (snowy.checked) {
+		fitImage(context, weather.cloudy.img);
+		fitImage(context, weather.snowy.img);
+	}
+	if (rainy.checked) {
+		fitImage(context, weather.rainy.img);
+	}
+}
 
 function resizeHandler(event) {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	//context.clearRect(0, 0, weatherCanvas.width, weatherCanvas.height);
+	weatherCanvas.width = .3*window.innerWidth;
+	weatherCanvas.height = .6*window.innerHeight;
+	//fitImage(context, weather.sunny.img, "s");
+	//fitImage(context, weather.cloudy.img);
+	//fitImage(context, weather.rainy.img);
+	//fitImage(context, weather.stormy.img);
+	//fitImage(context, weather.windy.img);
+	//fitImage(context, weather.hot.img, "s");
+	//fitImage(context, weather.cold.img, "s");
+	//fitImage(context, weather.snowy.img);
+	//fitImage(context, weather.rainy.img);
 	drawDate();
 }
 
-var birthdays = [];
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-var today = Date.now();
-var time = dateFormat(today, "h:MM TT");
-var timeFix = 0;
-var day = dateFormat(today, "dddd");
-var month = dateFormat(today, "mmmm");
-var date = dateFormat(today, "dS");
-var year = dateFormat(today, "yyyy");
-var monthNumber = parseInt(dateFormat(today, "m"));
-var season;
-var index = null;
-if (
-	monthNumber === 1 ||
-	monthNumber === 2 ||
-	monthNumber === 12
-) {
-	season = "winter (冬)";
-} else if (
-	monthNumber === 3 ||
-	monthNumber === 4 ||
-	monthNumber === 5
-) {
-	season = "spring (春)";
-} else if (
-	monthNumber === 6 ||
-	monthNumber === 7 ||
-	monthNumber === 8
-) {
-	season = "summer (夏)";
-} else if (
-	monthNumber === 9 ||
-	monthNumber === 10 ||
-	monthNumber === 11
-) {
-	season = "fall (秋)";
-}
-
-var days = {
-	"Sunday": "日",
-	"Monday": "月",
-	"Tuesday": "火",
-	"Wednesday": "水",
-	"Thursday": "木",
-	"Friday": "金",
-	"Saturday": "土"
-};
-var months = {
+const TRANSLATIONS = {
+	"Sunday": "日曜日",
+	"Monday": "月曜日",
+	"Tuesday": "火曜日",
+	"Wednesday": "水曜日",
+	"Thursday": "木曜日",
+	"Friday": "金曜日",
+	"Saturday": "土曜日",
 	"January": "1月",
 	"February": "2月",
 	"March": "3月",
@@ -69,70 +84,100 @@ var months = {
 	"September": "9月",
 	"October": "10月",
 	"November": "11月",
-	"December": "12月"
+	"December": "12月",
+	"winter": "冬",
+	"spring": "春",
+	"summer": "夏",
+	"fall": "秋"
 };
 
-var japaneseDates = [
-"一日",
-"二日",
-"三日",
-"四日",
-"五日",
-"六日",
-"七日",
-"八日",
-"九日",
-"十日",
-"十一日",
-"十二日",
-"十三日",
-"十四日",
-"十五日",
-"十六日",
-"十七日",
-"十八日",
-"十九日",
-"二十日",
-"二十一日",
-"二十二日",
-"二十三日",
-"二十四日",
-"二十五日",
-"二十六日",
-"二十七日",
-"二十八日",
-"二十九日",
-"三十日",
-"三十一日",
-"三十二日"
-];
-
-var searchText = "";
-
-function drawDate() {
-	today = Date.now() + 60000 * timeFix;
-	time = dateFormat(today, "h:MM TT");
-	//dateFormat(today, "h:MM TT, dddd, mmmm dS, yyyy");
-	context.fillStyle = "pink";
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = "purple";
-	fitText(context, time, 0, 0, canvas.width, canvas.height * 1/7);
-	context.fillStyle = "orange";
-	fitText(context, day + " (" + days[day] + ")", 0, canvas.height * 1/7, canvas.width, canvas.height * 1/7);
-	//fitText(context, days[day], canvas.width / 2, canvas.height * 1/6, canvas.width / 2, canvas.height * 1/6, .1, "l");
-	context.fillStyle = "blue";
-	//context.fillRect(0, canvas.height * 2/6, canvas.width, canvas.height * 1/6);
-	fitText(context, month + " (" + months[month] + ")", 0, canvas.height * 2/7, canvas.width, canvas.height * 1/7);
-	fitText(context, date + " (" + japaneseDates[parseInt(date) - 1] + ")", 0, canvas.height * 3/7, canvas.width, canvas.height * 1/7);
-	context.fillStyle = "red";
-	fitText(context, year, 0, canvas.height * 4/7, canvas.width, canvas.height * 1/7);
-	context.fillStyle = "green";
-	fitText(context, season, 0, canvas.height * 5/7, canvas.width, canvas.height * 1/7);
-	context.fillStyle = "light blue";
+const ASSETS = {
+	"sunny": "sunny.png",
+	"cloudy": "cloudy.png",
+	"rainy": "rainy.png",
+	"snowy": "snowy.png",
+	"windy": "windy.png",
+	"stormy": "stormy.png",
+	"hot": "hot.png",
+	"cold": "cold.png"
 }
 
+let today = Date.now();
+let time = dateFormat(today, "h:MM TT");
+let timeFix = 0;
+let day = dateFormat(today, "dddd");
+let month = dateFormat(today, "mmmm");
+let dateNumber = dateFormat(today, "d");
+let dateString = dateFormat(today, "dS");
+let year = dateFormat(today, "yyyy");
+let monthNumber = parseInt(dateFormat(today, "m"));
+let season;
+let divTime = document.getElementById("time");
+let divDayE = document.getElementById("dayE");
+let divDayJ = document.getElementById("dayJ");
+let divDateE = document.getElementById("dateE");
+let divDateJ = document.getElementById("dateJ");
+let divYear = document.getElementById("year");
+let divSeasonE = document.getElementById("seasonE");
+let divSeasonJ = document.getElementById("seasonJ");
+let cbSunny = document.getElementById("sunny");
+let cbCloudy = document.getElementById("cloudy");
+let cbRainy = document.getElementById("rainy");
+let cbSnowy = document.getElementById("snowy");
+let cbWindy = document.getElementById("windy");
+let cbStormy = document.getElementById("stormy");
+let cbHot = document.getElementById("hot");
+let cbCold = document.getElementById("cold");
+let weatherCanvas = document.getElementById("weatherCanvas");
+let context = weatherCanvas.getContext("2d");
+let loader = new Loader("images/");
+let weather = {};
+
+if (
+	monthNumber === 12 ||
+	monthNumber === 1 ||
+	monthNumber === 2
+) {
+	season = "winter";
+} else if (
+	monthNumber === 3 ||
+	monthNumber === 4 ||
+	monthNumber === 5
+) {
+	season = "spring";
+} else if (
+	monthNumber === 6 ||
+	monthNumber === 7 ||
+	monthNumber === 8
+) {
+	season = "summer";
+} else if (
+	monthNumber === 9 ||
+	monthNumber === 10 ||
+	monthNumber === 11
+) {
+	season = "fall";
+}
+
+console.log(time, day, month, dateString, year, monthNumber, season);
+
+Object.entries(ASSETS).forEach(function (pair) {
+	weather[pair[0]] = {
+		"render": true,
+		"img": loader.newImageAsset(pair[1], resizeHandler)
+	};
+});
+
 window.addEventListener("resize", resizeHandler, false);
+sunny.addEventListener("change", drawDate, false);
+cloudy.addEventListener("change", drawDate, false);
+rainy.addEventListener("change", drawDate, false);
+snowy.addEventListener("change", drawDate, false);
+windy.addEventListener("change", drawDate, false);
+stormy.addEventListener("change", drawDate, false);
+hot.addEventListener("change", drawDate, false);
+cold.addEventListener("change", drawDate, false);
 
 resizeHandler();
 drawDate()
-setInterval(function () {drawDate();}, 1000);
+//setInterval(function () {drawDate();}, 1000);
